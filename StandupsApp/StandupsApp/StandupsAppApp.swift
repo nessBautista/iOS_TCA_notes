fb40f83209079ce673f2da07591ae153c773209b
Ness <nestor.jbautista@gmail.com>
2024-12-07 13:43:10 -0600

    aur-220-Initial commit

---
+++ StandupsApp/StandupsApp/StandupsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StandupsAppApp.swift
+//  StandupsApp
+//
+//  Created by Ness on 07/12/24.
+//
+
+import SwiftUI
+
+@main
+struct StandupsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StandupsApp/StandupsAppTests/StandupsAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  StandupsAppTests.swift
+//  StandupsAppTests
+//
+//  Created by Ness on 07/12/24.
+//
+
+import Testing
+@testable import StandupsApp
+
+struct StandupsAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
